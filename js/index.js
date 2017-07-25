29baba1acb586e711f2d51c3ed3faea344fe4419
Melanie Balaz <melanie.j.balaz@gmail.com>
2017-07-25 09:40:11 +0200

    made navbar transition smoother

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
   $(window).scroll(function() { // check if scroll event happened
     var stop = Math.round($(window).scrollTop());
     if (stop > mainbottom) { // check if user scrolled more than 50 from top of the browser window
-      $("nav").show(); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+      $("nav").slideDown('fast'); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
     } else {
       $("nav").hide();
       //css("background-color", "transparent"); // if not, change it back to transparent
